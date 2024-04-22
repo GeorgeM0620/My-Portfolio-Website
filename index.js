49e74c832652cbe4bcb7c9d30f094327be879633
GeorgeM0620 <tshegofatsomolambo@gmail.com>
2024-04-23 00:56:59 +0200

    defining data types

---
+++ index.js
@@ -0,0 +1,26 @@
+//let
+let username = "George"
+console.log({ username});
+
+let surname = "Molambo"
+console.log({ surname });
+
+let age = 22;
+console.log({ age });
+
+let isWebdeveloper = true;
+console.log({ isWebdeveloper });
+
+let bio = " Hi am George Molambo, a WEB DESIGNER From Seshego, Polokwane in Limpopo. I create websites for businesses."
+console.log({ bio})
+
+// array of my educatuional background
+let educationalbackground = ['Matric/NSC Grade 12', 'Diploma in Public Relations N4-N6']
+console.log({ educationalbackground })
+console.log( educationalbackground.length )
+
+// array consisting of The projects I have worked on so far
+let listofProjectsIhaveWorkedOn = ['WEATHER APPLICATION', 'ONLINE BLOG WEBSITE FOR SOME LOCAL MUSIC ARTISTS', ' A COOKING SHOW APPLICATION', 'I have worked with various companies across South Africa namely, Afrihost, vox, Vodacom Business to name a few',]
+console.log({ listofProjectsIhaveWorkedOn });
+console.log( listofProjectsIhaveWorkedOn.length );
+
